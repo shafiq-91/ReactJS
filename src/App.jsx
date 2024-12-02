b69e8b8b15fcfc0dee521b1271340fabad5c5cf3
shafiq-91 <shajol234sj@gmail.com>
2024-12-02 21:26:27 +0600

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Nav />
     <Home />
+    <Footer />
     </>
   );
 }
